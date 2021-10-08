9993d78f394f8c693151a8690f843d338f983b3f
c0re100 <corehusky@gmail.com>
2021-10-09 03:36:54 +0800

    Bump to 3.1.9.1

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -26,4 +26,4 @@ constexpr auto AppVersion = 3001009;
 constexpr auto AppVersionStr = "3.1.9";
 constexpr auto AppBetaVersion = false;
 constexpr auto AppAlphaVersion = TDESKTOP_ALPHA_VERSION;
-constexpr auto AppVersionReleaseStr = "3.1.0.1";
+constexpr auto AppVersionReleaseStr = "3.1.9.1";

692c0bb8de02e8a324a6f551878d81ec9da39fab
c0re100 <corehusky@gmail.com>
2021-11-15 01:33:45 +0800

    Change to fullwidth | symbol

--- Telegram/SourceFiles/boxes/about_box.cpp
+++ Telegram/SourceFiles/boxes/about_box.cpp
@@ -146,6 +146,6 @@ QString currentVersionText() {
 	if (Platform::IsWindows64Bit()) {
 		result += " x64";
 	}
-	result += QString("|v%1").arg(UpstreamVersion);
+	result += QString("｜v%1").arg(UpstreamVersion);
 	return result;
 }

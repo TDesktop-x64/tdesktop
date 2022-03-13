3a2101a1f6c7d30cfc7932232911c69d23724a79
世界 <48624112+arm64v8a@users.noreply.github.com>
2022-03-13 23:37:05 +0800

    Show dc in media viewer (#64)

--- Telegram/SourceFiles/data/data_photo.h
+++ Telegram/SourceFiles/data/data_photo.h
@@ -161,6 +161,10 @@ public:
 
 	std::unique_ptr<Data::UploadState> uploadingData;
 
+	int32 getDC(){
+		return this->_dc;
+	};
+
 private:
 	QByteArray _inlineThumbnailBytes;
 	std::array<Data::CloudFile, Data::kPhotoSizeCount> _images;

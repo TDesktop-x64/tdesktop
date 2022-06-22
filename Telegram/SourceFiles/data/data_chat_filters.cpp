ddf2bbde32e7e49e50d866ca43b95b9e872072be
c0re100 <corehusky@gmail.com>
2022-06-22 18:17:23 +0800

    Tricky fix for folder drag and drop crash

--- Telegram/SourceFiles/data/data_chat_filters.cpp
+++ Telegram/SourceFiles/data/data_chat_filters.cpp
@@ -305,6 +305,7 @@ void ChatFilters::received(const QVector<MTPDialogFilter> &list) {
 		_loaded = true;
 		_listChanged.fire({});
 	}
+	remove(0); // to avoid folder drag and drop crash
 }
 
 void ChatFilters::apply(const MTPUpdate &update) {

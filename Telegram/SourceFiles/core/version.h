969890cc8cb491dc5e6fe82d7b4dbb857fe04877
c0re100 <corehusky@gmail.com>
2023-11-01 02:09:22 +0800

    Bump to 1.1
    Upstream version: 4.11.1

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -26,4 +26,4 @@ constexpr auto AppVersion = 1001000;
 constexpr auto AppVersionStr = "1.1";
 constexpr auto AppBetaVersion = false;
 constexpr auto AppAlphaVersion = TDESKTOP_ALPHA_VERSION;
-constexpr auto UpstreamVersion = "4.11";
+constexpr auto UpstreamVersion = "4.11.1";

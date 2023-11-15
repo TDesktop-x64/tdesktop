b665842577ed630d1ce88f6a0e19ca25fb1f8416
c0re100 <corehusky@gmail.com>
2023-11-15 22:34:01 +0800

    Fully remove pin to top

--- Telegram/SourceFiles/platform/platform_overlay_widget.cpp
+++ Telegram/SourceFiles/platform/platform_overlay_widget.cpp
@@ -50,7 +50,6 @@ public:
 	void updateState(
 		bool active,
 		bool maximized,
-		bool topState,
 		const style::WindowTitle &st) override;
 	void notifySynteticOver(Control control, bool over) override;
 
@@ -100,7 +99,6 @@ object_ptr<Ui::AbstractButton> DefaultOverlayWidgetHelper::Buttons::create(
 
 	const auto icon = [&] {
 		switch (control) {
-		case Control::OnTop: return &st::mediaviewTitleMinimize;
 		case Control::Minimize: return &st::mediaviewTitleMinimize;
 		case Control::Maximize: return &st::mediaviewTitleMaximize;
 		case Control::Close: return &st::mediaviewTitleClose;
@@ -169,7 +167,6 @@ object_ptr<Ui::AbstractButton> DefaultOverlayWidgetHelper::Buttons::create(
 void DefaultOverlayWidgetHelper::Buttons::updateState(
 		bool active,
 		bool maximized,
-		bool topState,
 		const style::WindowTitle &st) {
 	_maximized = maximized;
 }

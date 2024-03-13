3efcfacff2660f4a8a1574cc22faa34ea3d69909
c0re100 <corehusky@gmail.com>
2024-03-14 07:02:51 +0800

    Fix

--- Telegram/SourceFiles/history/history_item.cpp
+++ Telegram/SourceFiles/history/history_item.cpp
@@ -401,7 +401,7 @@ HistoryItem::HistoryItem(
 			setMedia(*media);
 			if (_media && _media->webpage()) {
 				if (isBlocked) {
-					_media->webpage()->applyChanges(WebPageType::Article, "", "", "", "", TextWithEntities(), FullStoryId(), nullptr, nullptr,  WebPageCollage(), 0, 0, "", 0);
+					_media->webpage()->applyChanges(WebPageType::Article, "", "", "", "", TextWithEntities(), FullStoryId(), nullptr, nullptr,  WebPageCollage(), nullptr, 0, 0, "", 0);
 				}
 			}
 		}

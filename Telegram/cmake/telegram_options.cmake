86680b2f7c10160de0d9b0dbdd6453c56a2931b1
c0re100 <corehusky@gmail.com>
2024-11-02 22:18:21 +0800

    Revert disable_autoupdate too

--- Telegram/cmake/telegram_options.cmake
+++ Telegram/cmake/telegram_options.cmake
@@ -36,9 +36,9 @@ if (TDESKTOP_API_ID STREQUAL "0" OR TDESKTOP_API_HASH STREQUAL "")
     " ")
 endif()
 
-#if (disable_autoupdate)
-#    target_compile_definitions(Telegram PRIVATE TDESKTOP_DISABLE_AUTOUPDATE)
-#endif()
+if (disable_autoupdate)
+    target_compile_definitions(Telegram PRIVATE TDESKTOP_DISABLE_AUTOUPDATE)
+endif()
 
 if (DESKTOP_APP_DISABLE_CRASH_REPORTS)
     target_compile_definitions(Telegram PRIVATE TDESKTOP_DISABLE_CRASH_REPORTS)
